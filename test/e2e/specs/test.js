96e5e3395a9f8de335093f1e5106692998825cb9
daniel-frank-miller <danielfrankmiller@gmail.com>
2024-10-20 16:21:39 -0400

    update 1

--- test/e2e/specs/test.js
+++ test/e2e/specs/test.js
@@ -6,7 +6,6 @@ var request = require('request')
 module.exports = {
   'game starts': function (browser) {
     // automatically uses dev Server port from /config.index.js
-    // default: http://localhost:8080
     // see nightwatch.conf.js
     const devServer = browser.globals.devServerURL
 
